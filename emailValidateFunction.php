e95608d417cd87943a07b8664c996656b5407bb3
olivergate <oliver.kg2@gmail.com>
2018-11-13 13:02:57 +0000

    doc bloc added

--- emailValidateFunction.php
+++ emailValidateFunction.php
@@ -1,16 +1,21 @@
 <?php
-
-
-
+/**Function emailValidate() validates emails based upon predefined PHP filter FILTER_VALIDATE_EMAIL
+ * if the email is valid it returns the email and if not it fires a redirect with a piece of get information that
+ * will throw an error on the given page
+ *
+ * @param $email input from the POST data request made by the user
+ * @return string returns a string wit a valid email or else redirects.
+ */
 function emailValidate ($email) : string {
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return $email;
-    } else {
-        header('Location: http://192.168.20.20/pokedex-2018-phbees/emailValidateFunction.php?invalid_email=1');
     }
+    return 'invalid email';
 }
 
 $email = 'email@bob.com';
+
+//test data
 var_dump(emailValidate($email));
 echo '<br>';
 $invalidEmail = '<script>Bob@didid.com';
