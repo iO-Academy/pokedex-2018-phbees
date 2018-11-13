1631a4852b0ee09dbf081593a01b5a3f4c29e4f0
Gideon Spektor <gidspektor@gmail.com>
2018-11-13 13:13:32 +0000

    half done, need to do an email check task first

--- session.php
+++ session.php
@@ -1,3 +1,13 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
 
+$session = new \theNamespace\session();
+
+
+
+
+//if(!isset($_SESSION['login'])) {
+//    header('location:');
+//    exit();
+//}
