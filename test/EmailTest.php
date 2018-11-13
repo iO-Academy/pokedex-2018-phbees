d24467c8eccedc68e84512ffcf6fc2c8744c5c90
olivergate <oliver.kg2@gmail.com>
2018-11-13 14:15:46 +0000

    test suite completed, not tested

--- test/EmailTest.php
+++ test/EmailTest.php
@@ -1,7 +1,31 @@
 <?php
 use PHPUnit\Framework\TestCase;
-
+require_once "../Email.php";
 class EmailTest extends TestCase
 {
-
+    public function testSuccess_emailValidate () : void
+    {
+        $email = new Email('bob@gmail.com');
+        $this->assertEquals($email, 'bob@gmail.com');
+    }
+    public function testFail_emailValidate () : void
+    {
+        $email = new Email('<script>bob@gmail.com');
+        if ($email!==null){
+            $result = 'object not instantiated';
+        } else {
+            $result = 'object instantiated';
+        }
+        $this->assertEquals($email, 'object not instantiated');
+    }
+    public function testTypeError_emailValidate () : void
+    {
+        $email = new Email(true);
+        if ($email!==null){
+            $result = 'object not instantiated';
+        } else {
+            $result = 'object instantiated';
+        }
+        $this->assertEquals($email, 'object not instantiated');
+    }
 }
