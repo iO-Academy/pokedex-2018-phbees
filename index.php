e4b48626adc78fad3e81215c6de2f709a5cbbb20
Gideon Spektor <gidspektor@gmail.com>
2018-11-13 11:21:51 +0000

    put an if statement in to make php sleep half way through the api call and then it puts it in an array

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
 
 /*
- * This is an API call to the pokeAPI and pulls all of the data from 151 pokemon
+ * This is an API call to the pokeAPI and pulls all of the data from 151 pokemon and puts them in an array
  *
- * @param this is the number 0
  *
  * @result this is a json string of all of the information of all of the 151 pokemon called
  */
-function grabApi ($number)
+function grabApi ()
 {
-    while ($number < 151) {
-        $number = $number + 1;
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'http://pokeapi.salestock.net/api/v2/pokemon/' . $number . '/');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        echo curl_exec($curl);
-        curl_close($curl);
+    $pokemon = [];
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $i = 0;
+    while ($i < 151) {
+        if ($i === 75){
+            sleep(60);
+        }
+        $i ++;
+        curl_setopt($curl, CURLOPT_URL, 'https://pokeapi.co/api/v2/pokemon/' . $i . '/');
+
+        $pokemon[] = curl_exec($curl);
+
     }
+    curl_close($curl);
+    return $pokemon;
 }
 
-grabApi(0);
+var_dump(grabApi());
