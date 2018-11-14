4f292a3d894dbfde3c9bf3c9404c3b6933b90e70
olivergate <oliver.kg2@gmail.com>
2018-11-14 15:49:45 +0000

    initial test commit

---
+++ src/test/UsersTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 14/11/2018
+ * Time: 15:49
+ */
+
+use theNamespace\Users;
+
+class UsersTest extends PHPUnit_Framework_TestCase
+{
+
+}
