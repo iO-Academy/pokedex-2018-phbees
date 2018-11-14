803a87e2ebec6f87eabf030c0b50082a553a07ee
Ariel Walker-Cochrane <arielwalkercochrane@gmail.com>
2018-11-14 09:40:36 +0000

    adding html and css files

---
+++ dex.php
@@ -0,0 +1,108 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>
+        PokeBees- Pokedex
+    </title>
+</head>
+<body>
+<h2>PokeBees Pokedex</h2>
+    <div class="container">
+        <div class="poke">
+            <div class="col_left">
+                <div class="pokeimg">
+                    <div>ID: 151</div>
+                </div>
+
+            </div>
+
+            <div class="col_right">
+                <h2>Name: x</h2>
+                <h3>Type: y</h3>
+                <h3>Type: y</h3>
+                <form>
+                    <input type="checkbox" name="seen">I've Seen This Pokemon<br>
+                    <input type="checkbox" name="seen">I've Caught This Pokemon<br>
+                    <input type="submit" value="Submit">
+                </form>
+
+            </div>
+        </div>
+
+        <div class="poke">
+            <div class="col_left">
+                <div class="pokeimg">
+                    <div>ID: 151</div>
+                </div>
+
+            </div>
+
+            <div class="col_right">
+                <h2>Name: x</h2>
+                <h3>Type: y</h3>
+                <h3>Type: y</h3>
+                <form>
+                    <input type="checkbox" name="seen">I've Seen This Pokemon<br>
+                    <input type="checkbox" name="seen">I've Caught This Pokemon<br>
+                    <input type="submit" value="Submit">
+                </form>
+
+            </div>
+        </div>
+
+        <div class="poke">
+            <div class="col_left">
+                <div class="pokeimg">
+                    <div>ID: 151</div>
+                </div>
+
+            </div>
+
+            <div class="col_right">
+                <h2>Name: x</h2>
+                <h3>Type: y</h3>
+                <h3>Type: y</h3>
+                <form>
+                    <input type="checkbox" name="seen">I've Seen This Pokemon<br>
+                    <input type="checkbox" name="seen">I've Caught This Pokemon<br>
+                    <input type="submit" value="Submit">
+                </form>
+
+            </div>
+        </div>
+
+        <div class="poke">
+            <div class="col_left">
+                <div class="pokeimg">
+                    <div>ID: 151</div>
+                </div>
+
+            </div>
+
+            <div class="col_right">
+                <h2>Name: x</h2>
+                <h3>Type: y</h3>
+                <h3>Type: y</h3>
+                <form>
+                    <input type="checkbox" name="seen">I've Seen This Pokemon<br>
+                    <input type="checkbox" name="seen">I've Caught This Pokemon<br>
+                    <input type="submit" value="Submit">
+                </form>
+
+            </div>
+        </div>
+        </div>
+    </div>
+
+<!--    --><?php
+//    foreach ($portfolioResults as $project) {
+//        echo '<img src="' . $project['image'] . '">';
+//    }
+//    ?>
+
+
+</body>
