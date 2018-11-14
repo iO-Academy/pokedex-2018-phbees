47f7383651b59a9fb3f0046effafc28b3afd6f36
Matt <matthew.tapley@hotmail.co.uk>
2018-11-14 13:54:39 +0000

    resolved code review requested changes. moved dbConn into a function in another folder and called it instead

--- script/php/dataScrape.php
+++ script/php/dataScrape.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once 'dbConn.php';
+
 /**
  * This is an API call to the pokeAPI and pulls all of the data from 151 pokemon and puts them in an array. The sleep stops the api call half way through for 60 seconds
- * $Temp stores each Pokemon data for a limited time, allowing for the needed data to be removed and stored in pokemon before being discarded on the next loop.
+ * $temp stores each Pokemon data for a limited time, allowing for the needed data to be removed and stored in pokemon before being discarded on the next loop.
  *
  * @return array $pokemon - holds all of our pokemon, with the only data we need from each, name, type 1 and type 2.
  */
-function grabApi () : array {
+function grabApi() : array {
     $pokemon = [];
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -15,12 +17,12 @@ function grabApi () : array {
         if ($i === 75){
             sleep(60);
         }
-        $i ++;
+        $i++;
         curl_setopt($curl, CURLOPT_URL, 'https://pokeapi.co/api/v2/pokemon/' . $i . '/');
         $resp = curl_exec($curl);
         $temp = json_decode($resp, TRUE);
         $name = $temp['forms'][0]['name'];
-        if (isset($temp['types'][1]['type']['name'])){
+        if (isset($temp['types'][1]['type']['name'])) {
             $type1 = $temp['types'][0]['type']['name'];
             $type2 = $temp['types'][1]['type']['name'];
             $pokemon[] = [$name, $type1, $type2];
@@ -36,45 +38,42 @@ function grabApi () : array {
 /**
  * Allows us to insert all of our data in our DB at once. By putting each into a () we can concatenate each and then insert into the db as one large value.
  *
- * @param array $allPokemon - the array of all the pokemon (with the only data we needed).
+ * @param array $pokemonArray - the array of all the pokemon (with the only data we needed).
  *
  * @return string $finalValues - our concatenated arrays with an trim ob the final Pokemon to remove the final ','.
  */
 
-function createSQLString (array $allPokemon) : string {
+function createSQLString(array $pokemonArray) : string {
     $values = '';
-    foreach ($allPokemon as $result){
+    foreach ($pokemonArray as $result) {
         $values .= '(\'' . $result[0] . '\',';
         $values .= '\'' . $result[1] . '\',';
         if (isset($result[2])) {
-           $values .= '\'' . $result[2] . '\'), ';
-       } else {
-           $values .= '\'' . NULL . '\'), ';
-       }
+            $values .= '\'' . $result[2] . '\'), ';
+        } else {
+            $values .= '\'' . NULL . '\'), ';
+        }
     }
-    $finalValues = rtrim($values, ', ');
-    return $finalValues;
+    return $finalValues = rtrim($values, ', ');
 }
 
 /**
- * inserts our Pokemon data in the database
+ * Inserts our Pokemon data in the database.
  *
  * @param string $finalValues - our concatenated arrays with an trim ob the final Pokemon to remove the final ','.
- * @param PDO $db - PDO to our database;
+ * @param PDO $db - PDO to our database.
  *
- * @return bool - true or false if the query was able to execute or not
+ * @return bool - true or false if the query was able to execute or not.
  */
-function insertIntoDatabase (string $finalValues, PDO $db) : bool {
-    $query = $db->prepare("INSERT INTO `pokemon` (`pokemon_name`, `pokemon_type`, `pokemon_type_2`) VALUES " . $finalValues .";");
+function insertIntoDatabase(string $finalValues, PDO $db) : bool {
+    $query = $db->prepare('INSERT INTO `pokemon` (`pokemon_name`, `pokemon_type`, `pokemon_type_2`) VALUES' . $finalValues .';');
     return $query->execute();
 }
 
-
 $pokemonArray = grabApi();
 
-$db = new PDO('mysql:host=127.0.0.1; dbname=Pokedex', 'root');
+$db = dbConn();
 
 $finalValues = createSQLString($pokemonArray);
 
 insertIntoDatabase($finalValues, $db);
-
