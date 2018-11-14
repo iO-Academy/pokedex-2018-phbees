53aed40f9c0e64188ed8e9de533eae782f2a0267
jaholt <holt.ja.halfways@gmail.com>
2018-11-14 12:08:02 +0000

    Fixed docblocs and psr2 things

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
-/*
- * This is an API call to the pokeAPI and pulls all of the data from 151 pokemon and puts them in an array. thew sleep stops the api call half way through for 60 seconds
- *$Temp stores each Pokemon data for a limited time, allowing for the needed data to be removed and stored in pokemon before being discarded on the next loop.
+/**
+ * This is an API call to the pokeAPI and pulls all of the data from 151 pokemon and puts them in an array. The sleep stops the api call half way through for 60 seconds
+ * $Temp stores each Pokemon data for a limited time, allowing for the needed data to be removed and stored in pokemon before being discarded on the next loop.
  *
- * $pokemon - holds all of our pokemon, with the only data we need from each, name, type 1 and type 2.
+ * @return array $pokemon - holds all of our pokemon, with the only data we need from each, name, type 1 and type 2.
  */
-
-function grabApi () {
+function grabApi () : array {
     $pokemon = [];
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -34,54 +33,48 @@ function grabApi () {
     return $pokemon;
 }
 
-/*
- * Allows us to insert all of our data in our DB at once. By putting each into a () we can concatinate each and then insert into the db as one large value.
+/**
+ * Allows us to insert all of our data in our DB at once. By putting each into a () we can concatenate each and then insert into the db as one large value.
  *
- *$allPokemen - the array of all the pokemon (with the only data we needed).
+ * @param array $allPokemon - the array of all the pokemon (with the only data we needed).
  *
- * $values - stores the concacinated arrays.
- * $finalValues - our concacinated arrays with an trim ob the final Pokemon to remove the final ','.
+ * @return string $finalValues - our concatenated arrays with an trim ob the final Pokemon to remove the final ','.
  */
 
-function insertintodatabase($allPokemon) {
+function createSQLString (array $allPokemon) : string {
     $values = '';
-
     foreach ($allPokemon as $result){
         $values .= '(\'' . $result[0] . '\',';
         $values .= '\'' . $result[1] . '\',';
         if (isset($result[2])) {
            $values .= '\'' . $result[2] . '\'), ';
        } else {
-           $values .= '\'' . 'NULL' . '\'), ';
+           $values .= '\'' . NULL . '\'), ';
        }
     }
-
-
     $finalValues = rtrim($values, ', ');
     return $finalValues;
 }
 
-
-/*
+/**
  * inserts our Pokemon data in the database
- * $finalValues - our concacinated arrays with an trim ob the final Pokemon to remove the final ','.
- * $db - PDO to our database;
+ *
+ * @param string $finalValues - our concatenated arrays with an trim ob the final Pokemon to remove the final ','.
+ * @param PDO $db - PDO to our database;
+ *
+ * @return bool - true or false if the query was able to execute or not
  */
-
-function database ($finalValues, $db){
-
+function insertIntoDatabase (string $finalValues, PDO $db) : bool {
     $query = $db->prepare("INSERT INTO `pokemon` (`pokemon_name`, `pokemon_type`, `pokemon_type_2`) VALUES " . $finalValues .";");
-
     return $query->execute();
-
 }
+
+
 $pokemonArray = grabApi();
 
 $db = new PDO('mysql:host=127.0.0.1; dbname=Pokedex', 'root');
 
-//$allPokemon = getPokeType($pokemonArray);
+$finalValues = createSQLString($pokemonArray);
 
-$finalValues = insertintodatabase($pokemonArray);
-
-database($finalValues, $db);
+insertIntoDatabase($finalValues, $db);
 
