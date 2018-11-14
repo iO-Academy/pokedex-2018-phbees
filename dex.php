66872fa3300c5190f02c6ae87e03697804685bc2
Daniel Coombes <daniel.coombes224@gmail.com>
2018-11-14 13:25:29 +0000

    removed checkbox and submit

--- dex.php
+++ dex.php
@@ -87,12 +87,6 @@
                 <h2>Name: x</h2>
                 <h3>Type: y</h3>
                 <h3>Type: y</h3>
-                <form>
-                    <input type="checkbox" name="seen">I've Seen This Pokemon<br>
-                    <input type="checkbox" name="seen">I've Caught This Pokemon<br>
-                    <input type="submit" value="Submit">
-                </form>
-
             </div>
         </div>
         </div>
