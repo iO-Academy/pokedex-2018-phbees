5a5bd7f89d205835c956c8380b4de1719afda81c
Daniel Coombes <daniel.coombes224@gmail.com>
2018-11-15 10:25:04 +0000

    Removed line breaks

--- dex.php
+++ dex.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $db = new PDO('mysql:host=127.0.0.1; dbname=pokedex', 'root');
 
-
 $Pokemon = new \theNamespace\classes\Pokemon($db);
 $Pokemon->fetchPokemonFromDatabase();
 $Pokemon->displayPokemon();
