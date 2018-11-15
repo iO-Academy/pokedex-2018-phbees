8c22fd9db53b9042622b0805ae9cd2fc90fd826a
jaholt <holt.ja.halfways@gmail.com>
2018-11-15 13:34:39 +0000

    Added docblocks

--- src/Classes/MySqlConnection.php
+++ src/Classes/MySqlConnection.php
@@ -9,11 +9,18 @@ class MySqlConnection implements Interfaces\Db
 
     private $dbConn;
 
+    /**
+     * MySqlConnection constructor. Assigns the database to the dbConn property on instantiation
+     */
     public function __construct()
     {
-        $this->dbConn = new \PDO('mysql:host=127.0.0.1;dbname=Pokedex','root');
+        $this->dbConn = new \PDO('mysql:host=127.0.0.1;dbname=pokedex','root');
     }
 
+    /**
+     * This function allows code outside the class to obtain a database connection
+     * @return \PDO returns the dbConn property that had the db connection assigned to it
+     */
     public function connect()
     {
         return $this->dbConn;
