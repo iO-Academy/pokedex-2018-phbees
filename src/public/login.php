ef6a2774470bbee2a568f1e603a4599ca2d4e852
olivergate <oliver.kg2@gmail.com>
2018-11-15 09:26:44 +0000

    deleted line that chagnes the object into a string as is unnecessary now

--- src/public/login.php
+++ src/public/login.php
@@ -6,7 +6,7 @@ try {
 } catch (UnexpectedValueException $e){
     header('Location:http://192.168.20.20/pokedex-2018-phbees/src/public/loginPage.php?invalid_email=1');
 }
-$cleanEmail = '(string) $email';
+
 $db = new PDO('mysql:host=127.0.0.1;dbname=Pokedex','root');
 $users = new \phbees\pokedex\Users($db,$email);
 $users->grabIdFromDb();
